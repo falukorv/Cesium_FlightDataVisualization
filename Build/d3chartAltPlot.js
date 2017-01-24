5fe3fc0ef117d672469a7a160eb97dac0cbbd382
Johan Berglund <johanbe8@kth.se>
2017-01-24 15:45:36 +0100

    Added the graph box and some example data

--- Build/d3chartAltPlot.js
+++ Build/d3chartAltPlot.js
@@ -1,27 +1,43 @@
-/*global d3, sharedObject */
+/*global d3, sharedObject, toGeoJSON */
 (function () {
     "use strict";
 
     // Various accessors that specify the four dimensions of data to visualize.
-    function x(d) { return d.income; }
-    function y(d) { return d.lifeExpectancy; }
-    function radius(d) { return d.population; }
-    function color(d) { return d.region; }
-    function key(d) { return d.name; }
+//    function x(d) { return distance[d]; }
+//    function y(d) { return altitude[d]; }
 
     // Chart dimensions.
     var margin = {top: 19.5, right: 19.5, bottom: 19.5, left: 39.5},
         width = 960 - margin.right,
         height = 500 - margin.top - margin.bottom;
-
-    // Various scales. These domains make assumptions of data, naturally.
-    var xScale = d3.scaleLinear([0,width]),
-        yScale = d3.scaleLinear([height,0]);
-
-    // The x & y axes.
-//    var xAxis = d3.svg.axis().orient("bottom").scale(xScale).ticks(12, d3.format(",d")),
-//        yAxis = d3.svg.axis().scale(yScale).orient("left");
         
+        
+    // Importing example flight data from a previous flight
+    var jsonData;
+    $.ajax({
+        url: '../SampleData/SPIDER_subsampled_trajectory.kml',
+//        url: '../SampleData/MASER13-trajectory.kml',
+        async: false
+    }).done(function(xml) {
+    jsonData = toGeoJSON.kml(xml);
+    });
+    
+    // Adding the data into new arrays, since the converted ones are quite messy
+    var altitude = [];
+    var distance = [];
+    for (var i = 0; i < jsonData.features[0].geometry.coordinates.length; i++) { 
+        altitude[i] = jsonData.features[0].geometry.coordinates[i][2];
+        distance[i] = jsonData.features[0].geometry.coordinates[i][1];
+    }
+        console.log(jsonData)
+    // Various scales.
+    var xScale = d3.scaleLinear().range([0,width]),
+        yScale = d3.scaleLinear().range([height,0]);
+        
+    xScale.domain([0,altitude.length]);
+    yScale.domain([0,d3.max(altitude)+d3.max(altitude)*0.1]);
+   
+    // The x & y axes.
     var xAxis = d3.axisBottom(xScale);
     var yAxis = d3.axisLeft(yScale);
 
@@ -42,6 +58,7 @@
     // Add the y-axis.
     svg.append("g")
         .attr("class", "y axis")
+        .attr("transform", "translate(20," + 0 + ")")
         .call(yAxis);
 
     // Add an x-axis label.
@@ -58,7 +75,7 @@
         .attr("text-anchor", "end")
         .attr("y", 6)
         .attr("dy", ".75em")
-        .attr("transform", "rotate(-90)")
+        .attr("transform", "rotate(-90) translate(0," + 20 + ")")
         .text("Altitude");
 
     // Add the altitude label; the value is set on transition.
@@ -67,107 +84,26 @@
         .attr("text-anchor", "start")
         .attr("y", 28)
         .attr("x", 30)
+        .attr("transform", "translate(20," + 0 + ")")
         .text('Altitude: High as a kite');
-
+    
+    // Add the apogege label
+    var apogee = Math.floor(d3.max(altitude));
     var apogee = svg.append("text")
         .attr("class", "apogee label")
         .attr("text-anchor", "end")
         .attr("x", width)
         .attr("y", 28)
-        .text("Apogee: En miljon mm");
+        .text("Apogee: " + apogee + "m");
+    
+    var line = d3.line()
+            .x(function(d,i){return xScale(i)})
+            .y(function(d){return yScale(d)})
+    
+//    console.log(altitude)
+    svg.append("path")
+        .attr("d", line(altitude))
+        .attr("stroke", "#fff")
+        .attr("stroke-width", "2px")
 
-//    // Load the data.
-//    d3.json("nations_geo.json", function(nations) {
-//
-//      // A bisector since many nation's data is sparsely-defined.
-//      var bisect = d3.bisector(function(d) { return d[0]; });
-//
-//      // Positions the dots based on data.
-//      function position(dot) {
-//        dot .attr("cx", function(d) { return xScale(x(d)); })
-//            .attr("cy", function(d) { return yScale(y(d)); })
-//            .attr("r", function(d) { return radiusScale(radius(d)); });
-//      }
-//
-//      // Defines a sort order so that the smallest dots are drawn on top.
-//      function order(a, b) {
-//        return radius(b) - radius(a);
-//      }
-//      // Interpolates the dataset for the given (fractional) year.
-//      function interpolateData(year) {
-//        sharedObject.yearData = nations.map(function(d) {
-//          return {
-//            name: d.name,
-//            region: d.region,
-//            income: interpolateValues(d.income, year),
-//            population: interpolateValues(d.population, year),
-//            lifeExpectancy: interpolateValues(d.lifeExpectancy, year),
-//            lat: d.lat,
-//            lon: d.lon
-//          };
-//        });
-//
-//        return sharedObject.yearData;
-//      }
-//
-//      // Add a dot per nation. Initialize the data at 1800, and set the colors.
-//      var dot = svg.append("g")
-//          .attr("class", "dots")
-//        .selectAll(".dot")
-//          .data(interpolateData(1800))
-//        .enter().append("circle")
-//          .attr("class", "dot")
-//          .style("fill", function(d) { return colorScale(color(d)); })
-//          .call(position)
-//          .sort(order)
-//		  .on("mouseover", function(d) { 
-//				sharedObject.dispatch.nationMouseover(d); 
-//		  })
-//          .on("click", function(d){
-//              sharedObject.flyTo(d);
-//          });
-//
-//      // Add a title.
-//      dot.append("title")
-//          .text(function(d) { return d.name; });
-//
-//
-//      // Tweens the entire chart by first tweening the year, and then the data.
-//      // For the interpolated data, the dots and label are redrawn.
-//      function tweenYear() {
-//        var year = d3.interpolateNumber(1800, 2009);
-//        return function(t) { displayYear(year(t)); };
-//      }
-//
-//      // Updates the display to show the specified year.
-//      function displayYear(year) {
-//        dot.data(interpolateData(year), key).call(position).sort(order);
-//        label.text(Math.round(year));
-//      }
-//
-//      // make displayYear global
-//      window.displayYear = displayYear;
-//
-//      // Finds (and possibly interpolates) the value for the specified year.
-//      function interpolateValues(values, year) {
-//        var i = bisect.left(values, year, 0, values.length - 1),
-//            a = values[i];
-//        if (i > 0) {
-//          var b = values[i - 1],
-//              t = (year - a[0]) / (b[0] - a[0]);
-//          return a[1] * (1 - t) + b[1] * t;
-//        }
-//        return a[1];
-//      }
-//
-//      sharedObject.dispatch.on("nationMouseover.d3", function(nationObject) {
-//          dot.style("fill", function(d) {
-//                 if (typeof nationObject !== 'undefined' && d.name === nationObject.name) {
-//                     return "#00FF00";
-//                 }
-//
-//                 return colorScale(color(d));
-//                 });
-//      });
-//    });
 }());
